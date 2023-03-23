6e24728236fde28991821ecc1fc19bf53315cd36
Sarah <21ftt1283@student.pb.edu.bn>
2023-03-23 11:27:49 +0800

    Revert Done logo to 21FTT1283

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,11 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center" style="color: red;">
+<<<<<<< HEAD
                     <h1>Done</h1>
+=======
+                    <h1>21FTT1283</h1>
+>>>>>>> parent of 5bc2abc... 21FTT1283 Logo changed to AS2
                 </div>
 
                 <div class="mt-16">
