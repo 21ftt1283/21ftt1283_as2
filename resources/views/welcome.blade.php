5bc2abc91906943d88266a19e0cbe4618ac54e25
Yasarah Yahya <98701586+21ftt1283@users.noreply.github.com>
2023-03-23 11:24:52 +0800

    21FTT1283 Logo changed to AS2
    Changed student ID (21FTT1283) logo to AS2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center" style="color: red;">
-                    <h1>21FTT1283</h1>
+                    <h1>AS2</h1>
                 </div>
 
                 <div class="mt-16">
